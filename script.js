d01e5c988a031f97e3db6b01930cc11e082e8d33
Janani <jananisri5637@gmail.com>
2025-09-26 15:16:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function timer(){
 
     const date = new Date();
     let hour = date.getHours();
-    let mer= hour % 12 ? "AM" : "PM";
-    hour=hour % 12 || 12;
+    const mer = hour >= 12 ? "PM" : "AM"; 
+    hour = hour % 12 || 12; 
     hour = hour.toString().padStart(2,0);
     let min = date.getMinutes().toString().padStart(2,0);
     let sec = date.getSeconds().toString().padStart(2,0);
@@ -14,6 +14,7 @@ function timer(){
 }
 timer();
 setInterval(timer,1000);
+
 
 
 
